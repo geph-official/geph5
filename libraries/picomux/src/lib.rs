a3638831a821d97d36449a6cd6674dbea629bf4c
nullchinchilla <nullchinchilla@pm.me>
2024-03-17 22:31:38 -0400

    Reduce MSS constant to 4096 bytes

--- libraries/picomux/src/lib.rs
+++ libraries/picomux/src/lib.rs
@@ -40,7 +40,7 @@ use crate::frame::{Header, PingInfo};
 
 const INIT_WINDOW: usize = 10;
 const MAX_WINDOW: usize = 500;
-const MSS: usize = 16384;
+const MSS: usize = 4096;
 
 #[derive(Clone, Copy, Debug)]
 pub struct LivenessConfig {
