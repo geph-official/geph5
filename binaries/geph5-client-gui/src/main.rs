469f4283602cf1923fe25aa8e1b10b338fd31e8d
nullchinchilla <nullchinchilla@pm.me>
2024-04-02 17:58:48 -0400

    fix

--- binaries/geph5-client-gui/src/main.rs
+++ binaries/geph5-client-gui/src/main.rs
@@ -15,8 +15,8 @@ fn main() {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 600.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([400.0, 300.0]),
         ..Default::default()
     };
     eframe::run_native(
