488cd0007fcd902f3b7dc132d7d608f26b89283d
xtex <xtexchooser@duck.com>
2024-05-14 10:00:36 +0000

    Remove database schema
    Per https://github.com/geph-official/geph5/pull/5#issuecomment-2108144682

--- binaries/geph5-broker/src/main.rs
+++ binaries/geph5-broker/src/main.rs
@@ -124,7 +124,6 @@ async fn main() -> anyhow::Result<()> {
     Lazy::force(&PLUS_MIZARU_SK);
     Lazy::force(&FREE_MIZARU_SK);
     Lazy::force(&database::POSTGRES);
-    database::init_schema().await?;
 
     let _gc_loop = Immortal::respawn(RespawnStrategy::Immediate, database_gc_loop);
 
