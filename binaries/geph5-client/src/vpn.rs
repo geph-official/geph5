2741aa6dbae14980d2e96383ffecd49c7a0532a6
nullchinchilla <nullchinchilla@pm.me>
2024-06-14 16:11:37 -0500

    shim out the vpn for mac

--- binaries/geph5-client/src/vpn.rs
+++ binaries/geph5-client/src/vpn.rs
@@ -10,3 +10,9 @@ mod windows;
 
 #[cfg(target_os = "windows")]
 pub use windows::*;
+
+#[cfg(target_os = "macos")]
+mod macos;
+
+#[cfg(target_os = "macos")]
+pub use macos::*;
---
+++ binaries/geph5-client/src/vpn/macos.rs
@@ -0,0 +1,24 @@
+use std::net::IpAddr;
+
+use anyctx::AnyCtx;
+use ipstack_geph::IpStack;
+
+use crate::Config;
+
+pub struct VpnCapture {
+    ipstack: IpStack,
+}
+
+impl VpnCapture {
+    pub fn new(ctx: AnyCtx<Config>) -> Self {
+        todo!()
+    }
+
+    pub fn ipstack(&self) -> &IpStack {
+        todo!()
+    }
+}
+
+pub fn vpn_whitelist(addr: IpAddr) {
+    todo!()
+}
