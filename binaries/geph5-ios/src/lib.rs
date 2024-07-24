9ebc880811736befb0ce57f2983e0be3afc8be7b
Lucas Meurer <lucasmeurer96@gmail.com>
2024-07-24 15:33:07 +0200

    Use my egui fork to get keyboard input working

--- binaries/geph5-ios/src/lib.rs
+++ binaries/geph5-ios/src/lib.rs
@@ -7,10 +7,14 @@ pub extern "C" fn start_app() {
 
 fn start() {
 
-    eframe::run_simple_native("geph5", Default::default(), |ctx, frame| {
+    let mut text = String::new();
+
+    eframe::run_simple_native("geph5", Default::default(), move |ctx, frame| {
 
         Window::new("Hello, world!").show(ctx, |ui| {
             ui.label("Hello, world!");
+
+            ui.text_edit_singleline(&mut text);
         });
 
     });
