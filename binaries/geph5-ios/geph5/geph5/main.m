c0e7dfc1f75d104e83c47ef08d8e3e5bf526ff72
Lucas Meurer <lucasmeurer96@gmail.com>
2024-07-24 15:27:07 +0200

    Link libgeph5.a, call start_app and add UIKit and Metal Frameworks

--- binaries/geph5-ios/geph5/geph5/main.m
+++ binaries/geph5-ios/geph5/geph5/main.m
@@ -1,18 +1,8 @@
-//
-//  main.m
-//  geph5
-//
-//  Created by Lucas Privat on 24.07.24.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#include "bindings.h"
 
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-    }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+    
+    start_app();
+    
+    return 0;
 }
