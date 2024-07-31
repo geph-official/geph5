5f1e11647bbcf2b3e25be90b921e29645bd5a9f8
nullchinchilla <nullchinchilla@pm.me>
2024-07-31 19:07:23 -0400

    fix

--- binaries/geph5-client/src/china/china-sync.sh
+++ binaries/geph5-client/src/china/china-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-racket generate-china.sh > china-domains.txt
+bash generate-china.sh > china-domains.txt
 curl https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt > china-ips.txt
