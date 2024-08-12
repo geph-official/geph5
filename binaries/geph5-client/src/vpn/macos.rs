ce3af646041bbcc88c45b5be3318303761414ece
nullchinchilla <nullchinchilla@pm.me>
2024-08-12 11:57:38 -0400

    fix

--- binaries/geph5-client/src/vpn/macos.rs
+++ binaries/geph5-client/src/vpn/macos.rs
@@ -5,20 +5,10 @@ use ipstack_geph::IpStack;
 
 use crate::Config;
 
-pub struct VpnCapture {
-    ipstack: IpStack,
-}
-
-impl VpnCapture {
-    pub fn new(ctx: AnyCtx<Config>) -> Self {
-        todo!()
-    }
-
-    pub fn ipstack(&self) -> &IpStack {
-        todo!()
-    }
-}
-
-pub fn vpn_whitelist(addr: IpAddr) {
-    // todo
+pub(super) async fn packet_shuffle(
+    ctx: AnyCtx<Config>,
+    send_captured: Sender<Bytes>,
+    recv_injected: Receiver<Bytes>,
+) -> anyhow::Result<()> {
+    todo!()
 }
