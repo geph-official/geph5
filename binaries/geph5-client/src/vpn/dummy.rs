e820fcb8a8a607d5ebd3135226f3bb3b66dbc3f6
nullchinchilla <nullchinchilla@pm.me>
2024-08-12 17:58:36 -0400

    Add todo! placeholder in packet_shuffle function in vpn/dummy.rs

--- binaries/geph5-client/src/vpn/dummy.rs
+++ binaries/geph5-client/src/vpn/dummy.rs
@@ -11,6 +11,7 @@ pub(super) async fn packet_shuffle(
     send_captured: Sender<Bytes>,
     recv_injected: Receiver<Bytes>,
 ) -> anyhow::Result<()> {
+    todo!()
 }
 
 pub fn vpn_whitelist(_addr: IpAddr) {
