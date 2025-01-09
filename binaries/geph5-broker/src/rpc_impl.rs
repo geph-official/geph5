90916c8cfdc489a2907e6beea456afc91722d856
nullchinchilla <nullchinchilla@pm.me>
2025-01-09 07:35:13 -0500

    tiemr

--- binaries/geph5-broker/src/rpc_impl.rs
+++ binaries/geph5-broker/src/rpc_impl.rs
@@ -44,10 +44,15 @@ impl RpcService for WrappedBrokerService {
         method: &str,
         params: Vec<serde_json::Value>,
     ) -> Option<Result<serde_json::Value, ServerError>> {
+        let start = Instant::now();
+        let resp = self.0.respond(method, params).await?;
         if let Some(client) = STATSD_CLIENT.as_ref() {
             client.count(&format!("broker.{method}"), 1).unwrap();
+            client
+                .time(&format!("broker_resptime.{method}"), start.elapsed())
+                .unwrap();
         }
-        self.0.respond(method, params).await
+        Some(resp)
     }
 }
 
