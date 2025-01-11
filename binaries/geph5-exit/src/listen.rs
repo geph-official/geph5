4772e8342455b0ad85e98a054576042e95325d72
nullchinchilla <nullchinchilla@pm.me>
2025-01-10 20:23:50 -0500

    Optimize IPv6 connection logic and update logging level

--- binaries/geph5-exit/src/listen.rs
+++ binaries/geph5-exit/src/listen.rs
@@ -54,7 +54,7 @@ async fn c2e_loop() -> anyhow::Result<()> {
             let remote_addr: SocketAddr = c2e_raw.remote_addr().unwrap().parse()?;
             if let SocketAddr::V4(remote_addr) = remote_addr {
                 let (asn, country) = ip_to_asn_country(*remote_addr.ip()).await?;
-                tracing::debug!(asn, country, remote_addr = display(remote_addr), "got ASN");
+                tracing::trace!(asn, country, remote_addr = display(remote_addr), "got ASN");
                 if CONFIG_FILE.wait().country_blacklist.contains(&country) {
                     anyhow::bail!(
                         "rejected connection from {remote_addr}/AS{asn} in blacklisted country {country}"
