9b15229233bb5e447b88d4e6123b2240a8b17760
nullchinchilla <nullchinchilla@pm.me>
2025-03-11 18:59:31 -0400

    Refactor RPC to use Arc for thread safety and async call handling

--- binaries/geph5-client/src/bin/geph5-client.rs
+++ binaries/geph5-client/src/bin/geph5-client.rs
@@ -1,4 +1,4 @@
-use std::{io::stdin, path::PathBuf};
+use std::{io::stdin, path::PathBuf, sync::Arc};
 
 use clap::Parser;
 use geph5_client::{logging, Client, Config};
@@ -24,14 +24,15 @@ fn main() -> anyhow::Result<()> {
     let config: serde_json::Value = serde_yaml::from_slice(&std::fs::read(args.config)?)?;
     let config: Config = serde_json::from_value(config)?;
     let client = Client::start(config);
-    let rpc = client.control_client().0;
+    let rpc = Arc::new(client.control_client().0);
     if args.stdio_rpc {
         std::thread::spawn(move || {
             let stdin = stdin();
             for line in stdin.lines() {
                 let line = line.unwrap();
                 let line: JrpcRequest = serde_json::from_str(&line).unwrap();
-                let resp = smolscale::block_on(rpc.call_raw(line)).unwrap();
+                let rpc = rpc.clone();
+                let resp = smolscale::block_on(async move {rpc.call_raw(line).await}).unwrap();
                 println!("{}", serde_json::to_string(&resp).unwrap());
             }
         });
