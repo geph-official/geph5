e29c0f1d59308403fc53fd1cc382ba46118e5fc8
nullchinchilla <nullchinchilla@pm.me>
2025-03-14 20:39:53 -0400

    Increase concurrency and set single-threaded mode in geph5-client

--- binaries/geph5-client/src/client_inner.rs
+++ binaries/geph5-client/src/client_inner.rs
@@ -115,7 +115,7 @@ static CONN_REQ_CHAN: CtxField<(
     (a, b)
 };
 
-pub static CONCURRENCY: usize = 1;
+pub static CONCURRENCY: usize = 3;
 
 #[tracing::instrument(skip_all)]
 pub async fn client_inner(ctx: AnyCtx<Config>) -> Infallible {
