cd3d55cbb8706b89327c8bb41ccd9c575db20874
thisbefruit <thisbefruit@protonmail.com>
2025-03-15 23:36:40 -0400

    fix horrifying bugs

--- binaries/geph5-client/src/lib.rs
+++ binaries/geph5-client/src/lib.rs
@@ -1,11 +1,16 @@
+use std::ffi::c_char;
+use std::ffi::c_int;
+use std::ffi::CStr;
+use std::io::Write;
+
 pub use broker::broker_client;
 pub use broker::BrokerSource;
 pub use client::Client;
 pub use client::{BridgeMode, BrokerKeys, Config};
 pub use control_prot::{ConnInfo, ControlClient};
-use nanorpc::JrpcId;
 use nanorpc::JrpcRequest;
 use nanorpc::RpcTransport;
+use once_cell::sync::OnceCell;
 pub use route::ExitConstraint;
 
 mod auth;
@@ -26,75 +31,56 @@ mod stats;
 mod taskpool;
 mod vpn;
 
-// C library for iOS
+// C interface
 
-use std::sync::Once;
-
-use serde_json::Value;
-static mut CLIENT: Option<Client> = None;
-static INIT: Once = Once::new();
+static CLIENT: OnceCell<Client> = OnceCell::new();
 
 #[no_mangle]
-pub extern "C" fn start_geph5_client(cfg: *const libc::c_char) -> libc::c_int {
-    // Convert C string to Rust string, return -1 on failure
-    let cfg_str = match unsafe { std::ffi::CStr::from_ptr(cfg) }.to_str() {
-        Ok(s) => s,
-        Err(_) => return -1,
-    };
+pub extern "C" fn start_client(cfg: *const c_char) -> libc::c_int {
+    let cfg_str = unsafe { CStr::from_ptr(cfg) }.to_str().unwrap();
+    let cfg: Config = serde_json::from_str(cfg_str).unwrap();
 
-    // Parse config, return -1 on failure
-    let cfg: Config = match serde_json::from_str(cfg_str) {
-        Ok(c) => c,
-        Err(_) => return -2,
-    };
+    CLIENT.get_or_init(|| Client::start(cfg));
 
-    unsafe {
-        INIT.call_once(|| {
-            CLIENT = Some(Client::start(cfg));
-        });
-    }
     0
 }
 
 #[no_mangle]
 pub extern "C" fn daemon_rpc(
-    method: *const libc::c_char,
-    params: *const libc::c_char,
-) -> *mut libc::c_char {
-    fn handle_rpc_call(
-        method: *const libc::c_char,
-        params: *const libc::c_char,
-    ) -> anyhow::Result<String> {
-        let method = unsafe { std::ffi::CStr::from_ptr(method) }
-            .to_str()?
-            .to_owned();
-        let params_str = unsafe { std::ffi::CStr::from_ptr(params) }.to_str()?;
-        let params: Vec<Value> = serde_json::from_str(params_str)?;
-        let jrpc = JrpcRequest {
-            jsonrpc: "2.0".into(),
-            method,
-            params,
-            id: JrpcId::Number(1),
-        };
+    jrpc_req: *const c_char,
+    out_buf: *mut c_char,
+    out_buflen: c_int,
+) -> c_int {
+    let req_str = unsafe { CStr::from_ptr(jrpc_req) }.to_str().unwrap();
+    let jrpc: JrpcRequest = serde_json::from_str(req_str).unwrap();
 
-        let client =
-            unsafe { CLIENT.as_ref() }.ok_or_else(|| anyhow::anyhow!("no client running"))?;
-        let ctrl = client.control_client();
-        let resp = smol::block_on(ctrl.0.call_raw(jrpc))?;
-        Ok(serde_json::to_string(&resp)?)
+    if let Some(client) = CLIENT.get() {
+        let ctrl = client.control_client().0;
+        if let Ok(response) = smolscale::block_on(async move { ctrl.call_raw(jrpc).await }) {
+            let response_json = serde_json::to_string(&response).unwrap();
+            let response_c = std::ffi::CString::new(response_json).unwrap();
+            let bytes = response_c.as_bytes_with_nul();
+
+            unsafe { fill_buffer(out_buf, out_buflen, bytes) }
+        } else {
+            -2 // jrpc error
+        }
+    } else {
+        -1 // daemon not started
     }
+}
 
-    let result = match handle_rpc_call(method, params) {
-        Ok(ok) => ok,
-        Err(e) => serde_json::to_string(&nanorpc::JrpcError {
-            code: -1,
-            message: e.to_string(),
-            data: serde_json::Value::Null,
-        })
-        .unwrap(),
-    };
-
-    std::ffi::CString::new(result)
-        .unwrap_or_else(|_| std::ffi::CString::new("null").unwrap())
-        .into_raw()
+unsafe fn fill_buffer(buffer: *mut c_char, buflen: c_int, output: &[u8]) -> c_int {
+    let mut slice = std::slice::from_raw_parts_mut(buffer as *mut u8, buflen as usize);
+    if output.len() < slice.len() {
+        if slice.write_all(output).is_err() {
+            tracing::debug!("writing to buffer failed!");
+            -4
+        } else {
+            output.len() as c_int
+        }
+    } else {
+        tracing::debug!(" buffer not big enough!");
+        -3
+    }
 }
