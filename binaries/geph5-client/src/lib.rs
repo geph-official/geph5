b8bc6c0d454b09a8e1e14f08d99a0d74bf8afb12
thisbefruit <thisbefruit@protonmail.com>
2025-03-16 00:12:21 -0400

    add test

--- binaries/geph5-client/src/lib.rs
+++ binaries/geph5-client/src/lib.rs
@@ -84,3 +84,112 @@ unsafe fn fill_buffer(buffer: *mut c_char, buflen: c_int, output: &[u8]) -> c_in
         -3
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use smol::Timer;
+
+    use super::*;
+    use std::{
+        ffi::CString,
+        net::{Ipv4Addr, SocketAddr},
+    };
+
+    const CONTROL_ADDR: SocketAddr =
+        SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12222);
+
+    pub const PAC_ADDR: SocketAddr =
+        SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12223);
+
+    const SOCKS5_ADDR: SocketAddr =
+        SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9909);
+
+    pub const HTTP_ADDR: SocketAddr =
+        SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9910);
+
+    #[test]
+    fn test_clib() {
+        let cfg = super::Config {
+            // These fields are the base defaults:
+            socks5_listen: Some(SOCKS5_ADDR),
+            http_proxy_listen: Some(HTTP_ADDR),
+            control_listen: Some(CONTROL_ADDR),
+            exit_constraint: super::ExitConstraint::Auto,
+            bridge_mode: BridgeMode::Auto,
+            cache: None,
+            broker: Some(BrokerSource::Race(vec![
+                BrokerSource::Fronted {
+                    front: "https://www.cdn77.com/".into(),
+                    host: "1826209743.rsc.cdn77.org".into(),
+                },
+                BrokerSource::Fronted {
+                    front: "https://vuejs.org/".into(),
+                    host: "svitania-naidallszei-2.netlify.app".into(),
+                },
+                BrokerSource::AwsLambda {
+                    function_name: "geph-lambda-bouncer".into(),
+                    region: "us-east-1".into(),
+                    access_key_id: String::from_utf8_lossy(
+                        &base32::decode(
+                            base32::Alphabet::Crockford,
+                            "855MJGAMB58MCPJBB97K4P2C6NC36DT8",
+                        )
+                        .unwrap(),
+                    )
+                    .to_string(),
+                    secret_access_key: String::from_utf8_lossy(
+                        &base32::decode(
+                            base32::Alphabet::Crockford,
+                            "8SQ7ECABES132WT4B9GQEN356XQ6GRT36NS64GBK9HP42EAGD8W6JRA39DTKAP2J",
+                        )
+                        .unwrap(),
+                    )
+                    .to_string(),
+                },
+            ])),
+            broker_keys: Some(BrokerKeys {
+                master: "88c1d2d4197bed815b01a22cadfc6c35aa246dddb553682037a118aebfaa3954".into(),
+                mizaru_free: "0558216cbab7a9c46f298f4c26e171add9af87d0694988b8a8fe52ee932aa754"
+                    .into(),
+                mizaru_plus: "cf6f58868c6d9459b3a63bc2bd86165631b3e916bad7f62b578cd9614e0bcb3b"
+                    .into(),
+            }),
+            // Values that can be overridden by `args`:
+            vpn: false,
+            spoof_dns: false,
+            passthrough_china: false,
+            dry_run: false,
+            credentials: geph5_broker_protocol::Credential::Secret(String::new()),
+            sess_metadata: Default::default(),
+            task_limit: None,
+            pac_listen: Some(PAC_ADDR),
+        };
+        let cfg_str = CString::new(serde_json::to_string(&cfg).unwrap()).unwrap();
+        let cfg_ptr = cfg_str.as_ptr();
+
+        let start_client_ret = start_client(cfg_ptr);
+        assert!(start_client_ret == 0);
+
+        // call daemon_rpc;
+        for _ in 0..2 {
+            let jrpc_req = JrpcRequest {
+                jsonrpc: "2.0".into(),
+                method: "user_info".into(),
+                params: [].into(),
+                id: nanorpc::JrpcId::Number(1),
+            };
+            let jrpc_req_str = CString::new(serde_json::to_string(&jrpc_req).unwrap()).unwrap();
+            let jrpc_req_ptr = jrpc_req_str.as_ptr();
+            // Allocate a buffer for the response
+            let mut out_buf = vec![0; 1024 * 128]; // Adjust size as needed
+            let out_buf_ptr = out_buf.as_mut_ptr();
+
+            let rpc_ret = daemon_rpc(jrpc_req_ptr, out_buf_ptr, out_buf.len() as _);
+            println!("daemon_rpc retcode = {rpc_ret}");
+            assert!(rpc_ret >= 0);
+            let output = unsafe { CStr::from_ptr(out_buf_ptr) }.to_str().unwrap();
+            println!("daemon_rpc output = {output}");
+            smolscale::block_on(async { Timer::after(std::time::Duration::from_secs(1)).await });
+        }
+    }
+}
