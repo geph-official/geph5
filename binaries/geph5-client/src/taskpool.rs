122b510dfcd03524d8b25e246dbb49233d5d8abf
nullchinchilla <nullchinchilla@pm.me>
2025-03-24 12:36:28 -0400

    fix

--- binaries/geph5-client/src/taskpool.rs
+++ binaries/geph5-client/src/taskpool.rs
@@ -10,7 +10,7 @@ static TASK_POOL: Lazy<Mutex<VecDeque<Task<anyhow::Result<()>>>>> =
 /// Add a task to the task pool. If the pool is full, the oldest task will be removed.
 pub fn add_task(task_limit: u32, task: Task<anyhow::Result<()>>) {
     let mut pool = TASK_POOL.lock();
-    if pool.len() >= task_limit as _ {
+    if pool.len() >= task_limit as usize {
         pool.pop_front();
     }
     pool.push_back(task);
