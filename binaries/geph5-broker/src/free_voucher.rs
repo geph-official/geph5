b8ad70bcc6ed36400db44a0237fd31a6f5f6403e
nullchinchilla <nullchinchilla@pm.me>
2025-05-30 23:01:51 -0400

    Refactor get_free_voucher to handle dynamic voucher generation and update logic

--- binaries/geph5-broker/src/free_voucher.rs
+++ binaries/geph5-broker/src/free_voucher.rs
@@ -2,23 +2,48 @@ use std::collections::BTreeMap;
 
 use geph5_broker_protocol::VoucherInfo;
 
-use crate::database::POSTGRES;
+use crate::{
+    database::POSTGRES,
+    payments::{PaymentClient, PaymentTransport},
+    CONFIG_FILE,
+};
 
 pub async fn get_free_voucher(user_id: i32) -> anyhow::Result<Option<VoucherInfo>> {
-    let row: Option<(String, String)> = sqlx::query_as(
-        "select voucher,description from free_vouchers natural join users where id = $1 limit 1",
+    loop {
+        let mut txn = POSTGRES.begin().await?;
+        let row: Option<(String, String)> = sqlx::query_as(
+        "select id, voucher,description from free_vouchers natural join users where id = $1 limit 1",
     )
     .bind(user_id)
-    .fetch_optional(&*POSTGRES)
+    .fetch_optional(&mut *txn)
     .await?;
-    if let Some((voucher, description)) = row {
-        let map: BTreeMap<String, String> = serde_json::from_str(&description)?;
-        Ok(Some(VoucherInfo {
-            code: voucher,
-            explanation: map,
-        }))
-    } else {
-        Ok(None)
+        if let Some((voucher, description)) = row {
+            if voucher == "" {
+                // dynamically generate one and save
+                // HACK: using the description to uniquely identify the row here.
+                let code = PaymentClient(PaymentTransport)
+                    .create_giftcard(CONFIG_FILE.wait().payment_support_secret.clone(), 1)
+                    .await?
+                    .map_err(|e| anyhow::anyhow!(e))?;
+                sqlx::query(
+                    "update free_vouchers set voucher = $1 where id = $2 and description = $3",
+                )
+                .bind(code)
+                .bind(user_id)
+                .bind(description)
+                .execute(&mut *txn)
+                .await?;
+                txn.commit().await?;
+            } else {
+                let map: BTreeMap<String, String> = serde_json::from_str(&description)?;
+                return Ok(Some(VoucherInfo {
+                    code: voucher,
+                    explanation: map,
+                }));
+            }
+        } else {
+            return Ok(None);
+        }
     }
 }
 
