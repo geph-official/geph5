24a08d640899bd997733340ee3e02a6f8292c5bf
nullchinchilla <nullchinchilla@pm.me>
2025-06-13 16:54:13 -0400

    Include ovh_de pool in bridge delay condition

--- binaries/geph5-broker/src/routes.rs
+++ binaries/geph5-broker/src/routes.rs
@@ -66,7 +66,11 @@ pub async fn bridge_to_leaf_route(
                     .await?
                 });
 
-                if bridge.pool.contains("waw") || country == "IR" || country == "TM" {
+                if bridge.pool.contains("waw")
+                    || bridge.pool.contains("ovh_de")
+                    || country == "IR"
+                    || country == "TM"
+                {
                     anyhow::Ok(RouteDescriptor::Delay {
                         milliseconds: delay_ms,
                         lower: tls_route!().into(),
