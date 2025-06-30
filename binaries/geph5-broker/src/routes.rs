2c6b6aecbfd18b05f0b9a926fa067b2a7551c7d6
nullchinchilla <nullchinchilla@pm.me>
2025-06-30 10:36:51 -0400

    Update bridge_to_leaf_route with ConnTest protocol wrapping Sosistab3New

--- binaries/geph5-broker/src/routes.rs
+++ binaries/geph5-broker/src/routes.rs
@@ -56,7 +56,7 @@ pub async fn bridge_to_leaf_route(
                     bridge_to_leaf_route_inner(
                         bridge.clone(),
                         exit.b2e_listen,
-                        ObfsProtocol::Sosistab3New(gencookie(), ObfsProtocol::None.into()).into()
+                        ObfsProtocol::ConnTest(ObfsProtocol::Sosistab3New(gencookie(), ObfsProtocol::None.into()).into()).into()
                     )
 
                 });
