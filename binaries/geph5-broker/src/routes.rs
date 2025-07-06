45ab04fe0306394d2843fe28dd9f32ec89e9ceca
nullchinchilla <nullchinchilla@pm.me>
2025-07-05 22:28:12 -0400

    Add tls_route and sosistab3_route to Race vector in bridge_to_leaf_route function

--- binaries/geph5-broker/src/routes.rs
+++ binaries/geph5-broker/src/routes.rs
@@ -93,7 +93,7 @@ pub async fn bridge_to_leaf_route(
                   {
                     return anyhow::Ok(RouteDescriptor::Delay {
                         milliseconds: delay_ms,
-                        lower: RouteDescriptor::Race(vec![meeklike_route!().await?.into()]).into(),
+                        lower: RouteDescriptor::Race(vec![meeklike_route!().await?.into(), tls_route!().await?.into(), sosistab3_route!().await?.into()]).into(),
                     })
                 }
                 // else if bridge.pool.contains("waw")
