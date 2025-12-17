fcc0ee27c98261c923859e79181622c59997e8ce
nullchinchilla <nullchinchilla@pm.me>
2025-12-17 07:15:40 -0500

    Revert "Re-enable request deduplication cache in rpc function"
    This reverts commit 8e5984393eb50410ef851ff713c2735d2e132148.

--- binaries/geph5-broker/src/main.rs
+++ binaries/geph5-broker/src/main.rs
@@ -210,25 +210,25 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn rpc(Json(payload): Json<JrpcRequest>) -> Json<JrpcResponse> {
-    // we assume the JrpcRequest IDs are reasonably unique, so we use this technique to deduplicate duplicate requests. Duplicate requests often happen when multiple different broker sources race against each other on censored networks.
-    static DEDUP_CACHE: LazyLock<Cache<(JrpcId, String), JrpcResponse>> = LazyLock::new(|| {
-        Cache::builder()
-            .time_to_live(Duration::from_secs(120))
-            .build()
-    });
+    // // we assume the JrpcRequest IDs are reasonably unique, so we use this technique to deduplicate duplicate requests. Duplicate requests often happen when multiple different broker sources race against each other on censored networks.
+    // static DEDUP_CACHE: LazyLock<Cache<(JrpcId, String), JrpcResponse>> = LazyLock::new(|| {
+    //     Cache::builder()
+    //         .time_to_live(Duration::from_secs(120))
+    //         .build()
+    // });
 
-    if rand::random::<f32>() < 0.001 {
-        tracing::debug!("{} entries in DEDUP_CACHE", DEDUP_CACHE.entry_count());
-    }
+    // if rand::random::<f32>() < 0.001 {
+    //     tracing::debug!("{} entries in DEDUP_CACHE", DEDUP_CACHE.entry_count());
+    // }
 
-    let resp = DEDUP_CACHE
-        .get_with(
-            (payload.id.clone(), payload.method.clone()),
-            WrappedBrokerService::new().respond_raw(payload),
-        )
-        .await;
+    // let resp = DEDUP_CACHE
+    //     .get_with(
+    //         (payload.id.clone(), payload.method.clone()),
+    //         WrappedBrokerService::new().respond_raw(payload),
+    //     )
+    //     .await;
 
-    // let resp = WrappedBrokerService::new().respond_raw(payload).await;
+    let resp = WrappedBrokerService::new().respond_raw(payload).await;
     Json(resp)
 }
 
