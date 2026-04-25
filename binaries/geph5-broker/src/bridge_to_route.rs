fdf7e51c84e01a5a01a3b657c60e2877ccdedbbd
nullchinchilla <nullchinchilla@pm.me>
2026-04-25 14:00:25 -0400

    Use sosistab3_protocol for non-Iran routes

--- binaries/geph5-broker/src/bridge_to_route.rs
+++ binaries/geph5-broker/src/bridge_to_route.rs
@@ -54,7 +54,7 @@ pub async fn bridge_to_leaf_route(
                     let fallback_protocol = if is_iran {
                         tls_protocol()
                     } else {
-                        tls_protocol()
+                        sosistab3_protocol()
                     };
                     let fallback_route = bridge_to_leaf_route_inner(
                         bridge.clone(),
